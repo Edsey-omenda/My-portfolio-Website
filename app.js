d0ac1ab67a9230d96f9c5c2156f2443fb7a38aa2
Edsey-omenda <omendajames0317@gmail.com>
2023-02-16 22:48:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Never give up!")
+
+const hamburger = document.querySelector("#hamburger")
+const menu = document.querySelector("#menu")
+
+hamburger.addEventListener("click", ()=>{
+    menu.classList.toggle("hidden")
+    hamburger.classList.toggle("bg-white")
+})
